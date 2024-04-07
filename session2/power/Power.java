ad3a32356ec40122d386b319195a5f18015cfd09
Niklas Walter <66417885+niklaswalter@users.noreply.github.com>
2024-04-07 15:01:04 +0200

    Add material for session 2.

---
+++ session2/power/Power.java
@@ -0,0 +1,52 @@
+package session2.power;
+
+/**
+ * This class gives an example of method overloading: computePower has two
+ * arguments, the base and the exponent. If the exponent is an int no problem,
+ * we just print the result. If the exponent is a double, we can get problems if
+ * the base is negative. If we just use Math.pow, we get NaN. Then we want to
+ * warn the user if this is the case. So we distinguish the two types of
+ * implementation (the one when the exponent is int and the one when the
+ * exponent is double) by Overloading.
+ *
+ */
+
+public class Power {
+
+	double result; // only definition, not initialization
+
+	
+	/**
+	 * It computes and prints base to the power of exponent, when exponent is
+	 * integer, so there are no problems if base is negative.
+	 *
+	 * @param base
+	 * @param exponent
+	 */
+	void computePower(double base, int exponent) {
+		//your code here
+		result = Math.pow(base, exponent);// note: Java method
+		System.out.println("Computing " + base + " to the power of " + exponent + ". The result is " + result);
+	}
+
+	/**
+	 * It computes and prints base to the power of exponent, when exponent is
+	 * double, so there can be problems if base is negative.
+	 *
+	 * @param base
+	 * @param exponent
+	 */
+	void computePower(double base, double exponent) {
+		// we distinguish the two cases
+		if (base >= 0 || exponent == Math.floor(exponent)) {
+			result = Math.pow(base, exponent);
+			System.out.println("Computing " + base + " to the power of " + exponent + ". The result is " + result);
+		} else {
+			// we warn the user!
+			System.out.println("Computing " + base + " to the power of " + exponent
+					+ ". You get a complex number: is that what you want?");
+		}
+	}
+
+	
+}
---
+++ session2/treesconstructor/TreeMain.java
@@ -0,0 +1,39 @@
+package session2.treesconstructor;
+
+/**
+ * This class gives a very simple example of the construction of an object
+ * through a constructor with arguments (here a double).
+ *
+ */
+
+public class TreeMain {
+
+	public static void main(String[] args) {
+		
+		double treeHeight = 3.2;
+
+		Tree myTree = new Tree(treeHeight); // call of the constructor with the double argument
+
+		//Tree myTree = new Tree();
+		//myTree.height = treeHeight;
+		
+		myTree.printHeight();
+
+		Tree mySecondTree = new Tree();
+		// try to see what happens if you now call
+		mySecondTree.printHeight();
+		/*
+		 * You see that height gets automatically initialized to zero as soon as the
+		 * object is created. If a primitive is a field in a class, on one hand it might
+		 * not be practical to force the user to initialize it to its appropriate value
+		 * before the data is used. On the other hand, it’s unsafe to leave it with a
+		 * garbage value, so each primitive field of a class is guaranteed to get an
+		 * initial value
+		 */
+		
+		//mySecondTree.aTreeField = new Tree(4.0);
+		
+		//mySecondTree.makeTheTreeFieldCallAMethod();
+
+	}
+}
